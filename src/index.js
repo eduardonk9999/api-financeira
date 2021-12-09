47214b6e614e9b2a2383b8c03eac0fc8f8fe6f6a
eduardonk9999 <thiago.novak7@gmail.com>
2021-12-09 11:53:10 -0300

    ADD, rota post

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-app.listen(3333)
+app.use(express.json());
+
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if(customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer Exists" });
+  }
+
+
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  })
+
+  return response.status(201).send();
+})
+
+
+
+
+app.listen(3333);
